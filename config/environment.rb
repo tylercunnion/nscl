ab4abe6f23d2614b38f63ebc2c82a16adcb1edc7
Tyler Cunnion <tyler.cunnion@gmail.com>
2009-02-04 19:41:58 +0000

    Upgrading to rails 2

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -42,6 +42,8 @@ Rails::Initializer.run do |config|
   # config.active_record.default_timezone = :utc
   
   # See Rails::Configuration for more options
+  
+  config.action_controller.session = { :session_key => "_nscl_session", :secret => "what brave new world we live in with such creatures in it" } 
 end
 
 # Add new inflection rules using the following format 
