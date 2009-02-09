b68b7f0f3fbae18721d293bdc08fc05232d9e360
Tyler Cunnion <tyler.cunnion@gmail.com>
2009-02-09 15:22:24 -0500

    Fixed routes for authentication
    Fixed stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
+
+  map.resources :users
+  map.resource :session
+
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
 
-  map.resource :session
   
   map.resources :publications, :controller => 'admin/publications', :path_prefix => "admin"
   map.resources :members, :controller => 'admin/members', :path_prefix => "admin"
