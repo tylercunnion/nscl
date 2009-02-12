ef1889c8542bfdc358deb8eecf1f1de7b0dcf9f0
Tyler Cunnion <tyler.cunnion@gmail.com>
2009-02-12 01:00:10 -0500

    Set up convention chairs page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,5 +3,12 @@ class EventsController < ApplicationController
   def index
     redirect_to :action => 'convention'
   end
-
+  
+  def chairs
+    @chairs = ["Ludi Chess", "Ludi Soccer", "Ludi Basketball", "Ludi Volleyball", "That's Entertainment", "Olympika Swimming", "Olympika Track Events", "Olympika Field Events", "Marathon", "Banquet"]
+  end
+  
+  def chair_signup
+    
+  end
 end
