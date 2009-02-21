5adcd2a970b0662020ad1c408465ee9381bcf1cb
Tyler Cunnion <tyler.cunnion@gmail.com>
2009-02-21 14:20:08 -0500

    Publication administration changes
    Hopefully editing an existing one won't delete the attachment now

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,11 @@
+Paperclip::Attachment.interpolations[:year] = proc do |attachment, style|
+  attachment.instance.year
+end
+
+Paperclip::Attachment.interpolations[:date] = proc do |attachment, style|
+  attachment.instance.date
+end
+
+Paperclip::Attachment.interpolations[:pub_type] = proc do |attachment, style|
+  attachment.instance.pub_type.gsub(' ', '_')
+end
