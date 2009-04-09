c734c57767ddeb84436f5100ba82e631ccb5f8ea
Tyler Cunnion <tyler.cunnion@gmail.com>
2009-04-09 14:03:41 -0400

    Changed methods to allow string arguments

--- app/models/spam_filter.rb
+++ app/models/spam_filter.rb
@@ -9,18 +9,21 @@ class SpamFilter
     }
   end
   
-  def train_spam(spamhash)
-    @m.system.train_spam spamhash.values.join(" ")
+  def train_spam(spam)
+    spam = spam.values.join(" ") if spam.class == Hash
+    @m.system.train_spam spam
     @m.take_snapshot
   end
   
-  def train_legit(legithash)
-    @m.system.train_legit legithash.values.join(" ")
+  def train_legit(legit)
+    legit = legit.values.join(" ") if legit.class == Hash
+    @m.system.train_legit legit
     @m.take_snapshot
   end
   
-  def is_legit?(hash)
-    return case @m.system.classify hash.values.join(" ")
+  def is_legit?(subject)
+    subject = subject.values.join(" ") if subject.class == Hash
+    return case @m.system.classify subject
     when "Legit":
       true
     when "Spam":
