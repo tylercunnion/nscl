b64e5ece17c576a4809bf4d38c6209bdb11ea47c
Tyler Cunnion <tyler.cunnion@gmail.com>
2009-05-21 10:25:02 -0400

    Changed some events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,7 +16,7 @@ class EventsController < ApplicationController
   
   def chairs
     @page_title = "Event Chairs"
-    @chairs = ["Ludi Chess", "Ludi Soccer", "Ludi Basketball", "Ludi Volleyball", "That's Entertainment", "Olympika Swimming", "Olympika Track Events", "Olympika Field Events", "Marathon", "Banquet"]
+    @chairs = ["Ludi Chess", "Ludi Soccer", "Ludi Kickball", "Ludi Volleyball", "That's Entertainment", "Olympika Swimming", "Olympika Track Events", "Olympika Field Events", "Marathon", "Banquet"]
   end
   
   def chairs_signup
