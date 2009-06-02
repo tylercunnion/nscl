be422b5911a4f0be7be26b18e57a159f8e705aec
Tyler Cunnion <tyler.cunnion@gmail.com>
2009-06-02 17:28:38 -0400

    Added Ultimate frisbee to events list.
    Eventually this must be spun off into a new DB table, when I get the time

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,7 +12,7 @@ class EventsController < ApplicationController
   
   def chairs
     @page_title = "Event Chairs"
-    @chairs = ["Ludi Chess", "Ludi Soccer", "Ludi Kickball", "Ludi Volleyball", "That's Entertainment", "Olympika Swimming", "Olympika Track Events", "Olympika Field Events", "Marathon", "Banquet"]
+    @chairs = ["Ultimate Frisbee", "Ludi Chess", "Ludi Soccer", "Ludi Kickball", "Ludi Volleyball", "That's Entertainment", "Olympika Swimming", "Olympika Track Events", "Olympika Field Events", "Marathon", "Banquet"]
   end
   
   def chairs_signup
