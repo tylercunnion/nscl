6bdef932f37a5d9c4fe2c97fe41c838f00e7d821
Tyler Cunnion <tyler.cunnion@gmail.com>
2009-12-01 15:03:01 -0500

    Google Analytics initializer

--- config/initializers/nscl.rb
+++ config/initializers/nscl.rb
@@ -2,3 +2,7 @@ PAGE_SIZE = 10.0
 HAL_CONTACT = APP_CONFIG['hal_contact']
 NSCL_SCHOL_CONTACT = APP_CONFIG['nscl_schol_contact']
 VP_ADDRESS = APP_CONFIG['vp_address']
+
+Rubaidh::GoogleAnalytics.tracker_id = APP_CONFIG['google_analytics']
+Rubaidh::GoogleAnalytics.domain_name = "nscl.org"
+Rubaidh::GoogleAnalytics.environments = ['production']
