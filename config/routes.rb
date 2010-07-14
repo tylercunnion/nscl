3a1e8e54e9dd47766bec0451cd50f9913008cceb
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-07-14 04:33:09 -0400

    Users might be part of the site in the future but for now there's no reason

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
+  #map.register '/register', :controller => 'users', :action => 'create'
+  #map.signup '/signup', :controller => 'users', :action => 'new'
 
   
   map.resources :publications, :controller => 'admin/publications', :path_prefix => "admin"
