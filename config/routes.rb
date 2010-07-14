9f2d4c4ff7540a4601535d9ce7bca1a073aa3186
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-07-14 04:31:02 -0400

    I see no reason why this was ever public

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.resources :users
+  #map.resources :users
   map.resource :session
 
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
